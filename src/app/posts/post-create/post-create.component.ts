541509f3c9f5f634d3ed9b86a666b5a0674e2972
Nguyen Duc Huy <nguyenduchuy1999@gmail.com>
2021-06-23 16:21:02 +0700

    Use ngForm, 2waybinding, templatedriven add posts

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,57 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Post } from '../../posts/post.model';
+
+// Decorator
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post.create.component.css']
+})
+
+export class PostCreateComponent {
+
+
+  // Using local reference
+  // newPost = 'NO CONTENT';
+  // onAddPost(postInput: HTMLTextAreaElement) {
+    //   console.dir(postInput);
+    //   his.newPost = postInput.value;
+    // }
+
+  // using twoway binding
+  // newPost = 'NO CONTENT';
+  // enteredValue = '';
+  // onAddPost(){
+  //   this.newPost = this.enteredValue;
+  // }
+
+  // using property and event binding to create new post
+  // enteredTitle = "";
+  // enteredContent = "";
+  // @Output() postCreated = new EventEmitter<Post>();
+
+  // onAddPost(){
+  //   const post: Post = {
+  //     title: this.enteredTitle,
+  //     content: this.enteredContent
+  //   };
+  //   this.postCreated.emit(post);
+  // }
+
+  // using form to add new post
+  enteredTitle = "";
+  enteredContent = "";
+  @Output() postCreated = new EventEmitter<Post>();
+
+  onAddPost(form: NgForm){
+    if (form.invalid){
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
+    };
+    this.postCreated.emit(post);
+  }
+}
